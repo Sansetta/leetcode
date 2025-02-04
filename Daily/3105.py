9fc9800f25705a2e3258004f9d526cd406008314
Shadrin Egor <94379717+Sansetta@users.noreply.github.com>
2025-02-04 20:08:12 +0500

    03.02.2025

---
+++ Daily/3105.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestMonotonicSubarray(self, nums):
+        max_increase = 1
+        max_decrease = 1
+        decrease = 1
+        increase = 1
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i + 1]:
+                increase += 1
+                max_increase = max(increase, max_increase)
+                decrease = 1
+
+            elif nums[i] < nums[i + 1]:
+                decrease += 1
+                max_decrease = max(decrease,max_decrease)
+                increase = 1
+
+            else:
+                increase = 1
+                decrease = 1
+
+
+        return max(max_increase,max_decrease)
+
+nums = [1,8,10,3,6]
+solve = Solution()
+print(solve.longestMonotonicSubarray(nums))
